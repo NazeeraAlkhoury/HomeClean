a9586549e05ff2bb62980d4dc1277b4ebee971b8
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-09 17:34:05 +0300

    Add Styles to AppTextStyles.

--- lib/core/utils/app_text_styles.dart
+++ lib/core/utils/app_text_styles.dart
@@ -1,6 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:homeclean/core/utils/app_colors.dart';
 
 abstract class AppTextStyles {
+  static TextStyle styleRegular20(BuildContext context) => TextStyle(
+        color: AppColors.darkBlueColor,
+        fontSize: getResponsiveFontSize(context, fontSize: 18),
+        fontWeight: FontWeight.normal,
+      );
+
+  static TextStyle styleRegular13(BuildContext context) => TextStyle(
+        color: AppColors.darkGreyColor,
+        fontSize: getResponsiveFontSize(context, fontSize: 13),
+        fontWeight: FontWeight.normal,
+      );
+
+  static TextStyle styleBold14(BuildContext context) => TextStyle(
+        color: AppColors.darkBlueColor,
+        fontSize: getResponsiveFontSize(context, fontSize: 14),
+        fontWeight: FontWeight.bold,
+      );
+
+  static TextStyle styleBold20(BuildContext context) => TextStyle(
+        color: AppColors.darkBlueColor,
+        fontSize: getResponsiveFontSize(context, fontSize: 20),
+        fontWeight: FontWeight.bold,
+      );
+
   static double getResponsiveFontSize(BuildContext context,
       {required double fontSize}) {
     double scaleFactor = getScaleFactor(context);
