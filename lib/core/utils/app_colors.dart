6239b96246773b5f9158313d6590646e9761620c
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-09 14:45:55 +0300

    Add color palette to AppColors.

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -2,4 +2,14 @@ import 'package:flutter/material.dart';
 
 abstract class AppColors {
   static const Color primaryColor = Color(0xff2EA3F2);
+  static const Color darkBlueColor = Color(0xff2E3271);
+  static const Color whiteColor = Color(0xffFAFCFF);
+  static const Color greenColor = Color(0xff79B542);
+  static const Color orangeColor = Color(0xffFFB400);
+  static const Color redColor = Color(0xffDC362E);
+  static const Color lightBlueColor = Color(0xffEAF0FE);
+  static const Color medBlueColor = Color(0xff9DB2CE);
+  static const Color darkGreyColor = Color(0xff938F96);
+  static const Color greyColor = Color(0xffCAC5CD);
+  static const Color lightGreyColor = Color(0xffE6E0E9);
 }
