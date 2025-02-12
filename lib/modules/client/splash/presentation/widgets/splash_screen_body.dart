504db6e05810e923ff0d51a3bd6c6d1d75c06446
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-12 17:43:38 +0300

    Create Splash feature.

---
+++ lib/modules/client/splash/presentation/widgets/splash_screen_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashScreenBody extends StatefulWidget {
+  const SplashScreenBody({super.key});
+
+  @override
+  State<SplashScreenBody> createState() => _SplashScreenBodyState();
+}
+
+class _SplashScreenBodyState extends State<SplashScreenBody> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
