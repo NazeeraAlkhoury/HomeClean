7fd3521185d2ef68795f795756d87c019b101cdc
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-13 18:51:26 +0300

    Create LocaleKeys

---
+++ lib/core/localization/locale_keys.dart
@@ -0,0 +1,26 @@
+abstract class LocaleKeys {
+  static const String terms = 'terms_of_use';
+  static const String agreeToProcess = 'agree_to_process';
+  static const String agreeContinue = 'agree_continue';
+  static const String exit = 'exit';
+  static const String login = 'login';
+  static const String enterMobileLogin = 'enter_mobile_login';
+  static const String next = 'next';
+  static const String noAccount = 'no_account';
+  static const String createAccount = 'create_account';
+  static const String sentLoginCode = 'sent_login_code';
+  static const String resendCode = 'resend_code';
+  //Create Account
+  static const String signUp = 'sign_up';
+  static const String fullName = 'full_name';
+  static const String mobileNumber = 'mobile_number';
+  static const String alreadyHaveAccount = 'already_have_account';
+  static const String confirmMobile = 'confirm_mobile';
+  static const String confirmNum = 'confirm_num';
+  static const String back = 'back';
+  static const String completeAccountInfo = 'complete_account_info';
+  static const String personalInfoSafe = 'personal_info_safe';
+  static const String idNumber = 'id_number';
+  static const String location = 'location';
+  static const String allowLocation = 'allow_location';
+}
