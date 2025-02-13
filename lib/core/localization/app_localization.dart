80f0a8f6ac7c6c89b1dd23fe9e549f74275978f6
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-13 19:37:03 +0300

    Create AppLocalization.

---
+++ lib/core/localization/app_localization.dart
@@ -0,0 +1,24 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+abstract class AppLocalization {
+  static const Locale arabicLang = Locale('ar', 'SA');
+  static const Locale englishLang = Locale('en', 'US');
+
+  static const Locale fallbackLocale = englishLang;
+  static const List<Locale> supportedLocales = [
+    englishLang,
+    arabicLang,
+  ];
+
+  static const String translationPath = 'assets/translations';
+
+  static Widget localizationWrapper({required Widget child}) {
+    return EasyLocalization(
+      supportedLocales: supportedLocales,
+      path: translationPath,
+      fallbackLocale: fallbackLocale,
+      child: child,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:homeclean/core/localization/app_localization.dart';
 import 'package:homeclean/core/utils/app_themes.dart';
 import 'package:homeclean/modules/client/splash/presentation/views/splash_view.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    AppLocalization.localizationWrapper(
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -14,6 +23,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'HomeClean',
       theme: AppThemes.lightTheme,
+      locale: context.locale,
+      supportedLocales: context.supportedLocales,
+      localizationsDelegates: context.localizationDelegates,
       home: const SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
