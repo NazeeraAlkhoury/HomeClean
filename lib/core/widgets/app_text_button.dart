cc1e8fda55dadbd92f4ffe4327e2f298527fa122
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-14 18:01:26 +0300

    Fix: remove unwanted spacing between text and TextButton in authentication screen.

--- lib/core/widgets/app_text_button.dart
+++ lib/core/widgets/app_text_button.dart
@@ -1,24 +1,33 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:homeclean/core/localization/locale_keys.dart';
 import 'package:homeclean/core/utils/app_colors.dart';
 import 'package:homeclean/core/utils/app_text_styles.dart';
 
 class AppTextButton extends StatelessWidget {
+  final String text;
   final Color textColor;
   final void Function()? onPressed;
   const AppTextButton({
     super.key,
     this.textColor = AppColors.darkGreyColor,
     this.onPressed,
+    required this.text,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
       onPressed: onPressed,
+      style: TextButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 4,
+          vertical: 6,
+        ),
+        minimumSize: Size.zero,
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        visualDensity: VisualDensity.compact,
+      ),
       child: Text(
-        LocaleKeys.exit.tr(),
+        text,
         style: AppTextStyles.styleRegular13(context).copyWith(
           color: textColor,
         ),
