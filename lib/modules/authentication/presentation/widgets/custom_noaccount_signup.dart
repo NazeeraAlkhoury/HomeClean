aad7fe96f48c43c54f9184b5583276c9fe39bff5
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-14 18:12:55 +0300

    Create CustomNoAccountSignUp.

---
+++ lib/modules/authentication/presentation/widgets/custom_noaccount_signup.dart
@@ -0,0 +1,33 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:homeclean/core/localization/locale_keys.dart';
+import 'package:homeclean/core/utils/app_colors.dart';
+
+import 'package:homeclean/core/utils/app_text_styles.dart';
+import 'package:homeclean/core/widgets/app_text_button.dart';
+
+class CustomNoAccountSignUp extends StatelessWidget {
+  const CustomNoAccountSignUp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          LocaleKeys.noAccount.tr(),
+          style: AppTextStyles.styleRegular13(context).copyWith(
+            color: AppColors.blackColor,
+          ),
+        ),
+        AppTextButton(
+          textColor: AppColors.primaryColor,
+          onPressed: () {},
+          text: LocaleKeys.createAccount.tr(),
+        ),
+      ],
+    );
+  }
+}
