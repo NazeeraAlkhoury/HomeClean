396950fac0d7bf399b780a918f778dbd2bb9f124
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-15 19:40:37 +0300

    Add OnBoarding to AppImageAssets.

--- lib/core/utils/app_image_assets.dart
+++ lib/core/utils/app_image_assets.dart
@@ -2,4 +2,7 @@ abstract class AppImageAssets {
   static const String logo = 'assets/images/logo.png';
   static const String mobileLogin = 'assets/images/mobile_login.png';
   static const String privacyPolicy = 'assets/images/privacy_policy.png';
+  static const String onBoarding1 = 'assets/images/onboarding1.png';
+  static const String onBoarding2 = 'assets/images/onboarding2.png';
+  static const String onBoarding3 = 'assets/images/onboarding3.png';
 }
--- lib/modules/client/onboarding/widgets/onboarding_view_body.dart
+++ lib/modules/client/onboarding/widgets/onboarding_view_body.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:homeclean/core/utils/app_image_assets.dart';
 
 class OnboardingViewBody extends StatelessWidget {
   const OnboardingViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Image.asset(AppImageAssets.logo),
+      ],
+    );
   }
 }
