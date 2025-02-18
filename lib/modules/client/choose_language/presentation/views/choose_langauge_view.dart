39171db26aa6dafb54f3ae9031bc43da0f15f1cc
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-18 16:40:08 +0300

    Create ChooseLangaugeView.

---
+++ lib/modules/client/choose_language/presentation/views/choose_langauge_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChooseLangaugeView extends StatelessWidget {
+  const ChooseLangaugeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
